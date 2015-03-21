45fbbdcc42bd341ab40bc7c5edbdf510a71c188d
Facundo <rodrig89@gmail.com>
2015-03-21 12:34:25 -0300

    commit 5

--- app/src/main/java/com/example/facundo/recorridaszotp/MainActivity.java
+++ app/src/main/java/com/example/facundo/recorridaszotp/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
-    //comentario de prueba2
+    //comentario de prueba4
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
