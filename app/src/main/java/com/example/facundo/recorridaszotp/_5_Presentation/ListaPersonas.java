63a53804adfcfbbbb87dc004f3a5350e63460856
Gonzalo89 <gonzalo89@ymail.com>
2015-09-05 15:05:46 -0300

    Falta que el boton habra el menu

--- app/src/main/java/com/example/facundo/recorridaszotp/_5_Presentation/ListaPersonas.java
+++ app/src/main/java/com/example/facundo/recorridaszotp/_5_Presentation/ListaPersonas.java
@@ -1,9 +1,12 @@
 package com.example.facundo.recorridaszotp._5_Presentation;
 
+import android.content.ClipData;
 import android.content.Context;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,9 +16,11 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.facundo.recorridaszotp.R;
+import com.example.facundo.recorridaszotp._1_Infraestructure.AdaptadorListaMenu;
 import com.example.facundo.recorridaszotp._1_Infraestructure.AdaptadorListaPersonas;
 import com.example.facundo.recorridaszotp._1_Infraestructure.DBUtils;
 import com.example.facundo.recorridaszotp._2_DataAccess.PersonaDataAccess;
+import com.example.facundo.recorridaszotp._3_Domain.ItemLista;
 import com.example.facundo.recorridaszotp._3_Domain.Persona;
 
 import java.util.ArrayList;
@@ -24,8 +29,9 @@ import java.util.List;
 public class ListaPersonas extends ActionBarActivity {
     private DrawerLayout navDrawerLayout;
     private ListView navList;
-    private ArrayList<String> navItms;
-    ArrayAdapter<String> navAdapter;
+    private ArrayList<ItemLista> navItms;
+    AdaptadorListaMenu navAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,15 +64,49 @@ public class ListaPersonas extends ActionBarActivity {
         //Establecemos header
         navList.addHeaderView(header);
 
-        navItms = new ArrayList<String>();
-        ArrayList<String> lista = new ArrayList<String>();
-        lista.add("Mapa");
-        lista.add("Segundo elemento");
-        navAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, lista);
+        navItms = new ArrayList<ItemLista>();
+        navItms.add(new ItemLista("Mapa"));
+        navItms.add(new ItemLista("Segundo elemento"));
+        navAdapter = new AdaptadorListaMenu(this, navItms);
         navList.setAdapter(navAdapter);
+
+        //Declaramos el mDrawerToggle y las imgs a utilizar
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                navDrawerLayout,         /* DrawerLayout object */
+                               ///R.drawable.ic_drawer,  /* Icono de navegacion*/
+                R.string.app_name,  /* "open drawer" description */
+                R.string.hello_world  /* "close drawer" description */
+        ) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                Log.e("Cerrado completo", "!!");
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                Log.e("Apertura completa", "!!");
+            }
+        };
+
+        // Establecemos que mDrawerToggle declarado anteriormente sea el DrawerListener
+        navDrawerLayout.setDrawerListener(mDrawerToggle);
+        //Establecemos que el ActionBar muestre el Boton Home
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        //Establecemos la accion al clickear sobre cualquier item del menu.
+        //De la misma forma que hariamos en una app comun con un listview.
+        navList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long id) {
+                //  MostrarFragment(position);
+            }
+        });
     }
 
-    @Override
+ /*   @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_lista_personas, menu);
@@ -86,5 +126,5 @@ public class ListaPersonas extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
