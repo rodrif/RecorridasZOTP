6849becac76dd5957967ac93447a9228743da726
Gonzalo89 <gonzalo89@ymail.com>
2016-02-01 20:33:11 -0300

    arreglo fecha de nacimiento

--- app/src/main/java/com/example/facundo/recorridaszotp/_0_Infraestructure/JsonUtils/PersonaJsonUtils.java
+++ app/src/main/java/com/example/facundo/recorridaszotp/_0_Infraestructure/JsonUtils/PersonaJsonUtils.java
@@ -48,7 +48,7 @@ public class PersonaJsonUtils extends BasicJsonUtil<Persona> {
         persona.setEstado(personaJson.optInt("estado"));
         persona.setZonaByWebId(personaJson.optInt("web_zone_id"));
         if (personaJson.optString("fecha_nacimiento") != "null") {
-            persona.setFechaNacimiento(personaJson.optString("fecha_nacimiento"));
+            persona.setFechaNacimientoDesdeWeb(personaJson.optString("fecha_nacimiento"));
         }
         if (personaJson.optString("descripcion") != "null") {
             persona.setObservaciones(personaJson.optString("descripcion"));
