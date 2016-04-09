cc459168e5d64437e2230a1dc2ba97c69888507d
Gonzalo <gonzalo89@ymail.com>
2016-04-09 15:19:46 -0300

    new class Roles

--- app/src/main/java/com/example/facundo/recorridaszotp/_3_Domain/Roles.java
+++ app/src/main/java/com/example/facundo/recorridaszotp/_3_Domain/Roles.java
@@ -4,4 +4,25 @@ package com.example.facundo.recorridaszotp._3_Domain;
  * Created by gonzalo on 09/04/16.
  */
 public class Roles {
+    static private Roles instance = null;
+    static public Roles getInstance() {
+        if (instance == null) {
+            instance = new Roles();
+        }
+        return instance;
+    }
+
+    static private String getPermissions(int rolId) {
+        String resultado = "";
+        switch (rolId) {
+            case 1:
+                resultado = "CrearUsuario,EditarUsuario";
+                break;
+        }
+        return resultado;
+    }
+
+    static public boolean getPermission(String permiso) {
+            return false;
+    }
 }
