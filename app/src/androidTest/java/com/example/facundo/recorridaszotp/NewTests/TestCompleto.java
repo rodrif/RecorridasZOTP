d3892a8869a5f2018c912216df3c963be0c2398d
Gonzalo <gonzalo89@ymail.com>
2016-06-05 23:32:15 -0300

    Test Suite

---
+++ app/src/androidTest/java/com/example/facundo/recorridaszotp/NewTests/TestCompleto.java
@@ -0,0 +1,17 @@
+package com.example.facundo.recorridaszotp.NewTests;
+
+/**
+ * Created by gonzalo on 05/06/16.
+ */
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+   PersonTests.class
+})
+
+public class TestCompleto {
+    // the class remains empty,
+    // used only as a holder for the above annotations
+}
