43d51b2132c2644f0ec284a93c969585d309d9e1
Gonzalo <gonzalo89@ymail.com>
2016-08-28 15:07:51 -0300

    Se centra en la zona al sleccionar la misma

--- app/src/main/java/com/example/facundo/recorridaszotp/_0_Infraestructure/ZonaPersonaListener.java
+++ app/src/main/java/com/example/facundo/recorridaszotp/_0_Infraestructure/ZonaPersonaListener.java
@@ -9,6 +9,8 @@ import com.example.facundo.recorridaszotp._2_DataAccess.RanchadaDataAccess;
 import com.example.facundo.recorridaszotp._3_Domain.Familia;
 import com.example.facundo.recorridaszotp._3_Domain.Ranchada;
 import com.example.facundo.recorridaszotp._5_Presentation.PersonaFragment;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
 import java.util.List;
 
@@ -24,7 +26,7 @@ public class ZonaPersonaListener implements AdapterView.OnItemSelectedListener {
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String stringZona = (String)parent.getItemAtPosition(position);
+        final String stringZona = (String)parent.getItemAtPosition(position);
         if (this.personaFragment.getAdaptadorRanchada() != null) {
             this.personaFragment.getAdaptadorRanchada().clear();
             List<Ranchada> ranchadas = RanchadaDataAccess.get().filtrarPorZona(stringZona);
@@ -45,7 +47,12 @@ public class ZonaPersonaListener implements AdapterView.OnItemSelectedListener {
                 }
             this.personaFragment.actualizarFamilia();
         }
-        this.personaFragment.getMapFragmentPersona().getMapAsync(this.personaFragment);
+        this.personaFragment.getMapFragmentPersona().getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                ZonaDrawer.centrarEnZona(googleMap, stringZona);
+            }
+        });
     }
 
     @Override
