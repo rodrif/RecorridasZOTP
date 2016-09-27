97438c85967115dbc8e62d117803657d8181d6d6
Gonzalo <gonzalo89@ymail.com>
2016-09-26 22:41:34 -0300

    agrego area a config

--- app/src/main/java/com/example/facundo/recorridaszotp/_2_DataAccess/Config.java
+++ app/src/main/java/com/example/facundo/recorridaszotp/_2_DataAccess/Config.java
@@ -11,6 +11,7 @@ import java.util.Map;
 public class Config {
     private static Config instance = null;
     private int rol = -1;
+    private int area = -1;
     private int userWebId;
  //   private String userMail = "rodrif89@gmail.com"; 123456789
     private String userMail = "";
@@ -131,6 +132,14 @@ public class Config {
 
     public void setIsEditing(boolean isEditing) {
         this.isEditing = isEditing;
+    }
+
+    public int getArea() {
+        return area;
+    }
+
+    public void setArea(int area) {
+        this.area = area;
     }
 
     public void logOut() {
