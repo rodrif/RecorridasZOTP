e906b6b58e986a5d89760ae8ea0682b3e721224a
gonzalo <gonzalo@localhost.localdomain>
2017-09-12 23:32:33 -0300

    Version nuevo servidor v2.0.0(33)

--- app/src/main/java/com/example/facundo/recorridaszotp/_0_Infraestructure/Utils.java
+++ app/src/main/java/com/example/facundo/recorridaszotp/_0_Infraestructure/Utils.java
@@ -99,7 +99,7 @@ public final class Utils {
     public static final String NOTIFICACION = "Notificación";
     public static final String PERSONA_ID = "persona_id";
     public static final String AREA_ID = "area_id";
-    public static final String VERSION = "32";
+    public static final String VERSION = "33";
 
     public static String toString(InputStream inputStream) throws Exception {
         BufferedReader r = new BufferedReader(new InputStreamReader(inputStream));
--- app/src/main/java/com/example/facundo/recorridaszotp/_1_Red/Notificaciones/RegistrationIntentService.java
+++ app/src/main/java/com/example/facundo/recorridaszotp/_1_Red/Notificaciones/RegistrationIntentService.java
@@ -98,18 +98,18 @@ public class RegistrationIntentService extends IntentService {
     // [START subscribe_topics]
     private void subscribeOrUnsuscribeTopics(String token) throws IOException {
         GcmPubSub pubSub = GcmPubSub.getInstance(this);
-        pubSub.unsubscribe(token, "/topics/" + Utils.ROL_ADMIN_STRING + "-dev");
-        pubSub.unsubscribe(token, "/topics/" + Utils.ROL_REFERENTE_STRING + "-dev");
-        pubSub.unsubscribe(token, "/topics/" + Utils.ROL_COORDINADOR_STRING + "-dev");
-        pubSub.unsubscribe(token, "/topics/" + Utils.ROL_VOLUNTARIO_STRING + "-dev");
-        pubSub.unsubscribe(token, "/topics/" + Utils.ROL_INVITADO_STRING + "-dev");
+        pubSub.unsubscribe(token, "/topics/" + Utils.ROL_ADMIN_STRING);
+        pubSub.unsubscribe(token, "/topics/" + Utils.ROL_REFERENTE_STRING);
+        pubSub.unsubscribe(token, "/topics/" + Utils.ROL_COORDINADOR_STRING);
+        pubSub.unsubscribe(token, "/topics/" + Utils.ROL_VOLUNTARIO_STRING);
+        pubSub.unsubscribe(token, "/topics/" + Utils.ROL_INVITADO_STRING);
         if (Config.getInstance().isLoginOk()) {
             ArrayList<String> TOPICS = new ArrayList<String>();
             int rolId = Config.getInstance().getRol();
             TOPICS.add(Roles.getInstance().getRoleName(rolId));
             for (String topic : TOPICS) {
-                Log.d(Utils.APPTAG, "Suscrito a topic: " + topic + "-dev");
-                pubSub.subscribe(token, "/topics/" + topic + "-dev", null);
+                Log.d(Utils.APPTAG, "Suscrito a topic: " + topic);
+                pubSub.subscribe(token, "/topics/" + topic, null);
             }
         }
     }
